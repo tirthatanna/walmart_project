15f965cc3a90b62a10926d5b54ea2897cb292768
tirthatanna <125922058+tirthatanna@users.noreply.github.com>
2023-06-24 16:08:08 +0100

    wamart_queries.sql

--- wamart_queries.sql
+++ wamart_queries.sql
@@ -49,6 +49,29 @@ FROM total_sales
 ORDER BY total_sales DESC
 LIMIT 3;
 
+-- View: store_growth_rate
+
+CREATE OR REPLACE VIEW wal.store_growth_rate
+ AS
+ SELECT subquery.store_id,
+    (subquery.sales_2012 - subquery.sales_2010) / subquery.sales_2010 AS growth_rate
+   FROM ( SELECT walmart_store_sales.store_id,
+            sum(
+                CASE
+                    WHEN EXTRACT(year FROM walmart_store_sales.date) = 2010::numeric THEN walmart_store_sales.weekly_sales
+                    ELSE 0::numeric
+                END) AS sales_2010,
+            sum(
+                CASE
+                    WHEN EXTRACT(year FROM walmart_store_sales.date) = 2012::numeric THEN walmart_store_sales.weekly_sales
+                    ELSE 0::numeric
+                END) AS sales_2012
+           FROM wal.walmart_store_sales
+          GROUP BY walmart_store_sales.store_id) subquery;
+
+ALTER TABLE wal.store_growth_rate
+    OWNER TO postgres;
+
 -- Which store has the highest growth rate from 2010 to 2012?
 SELECT store_id, growth_rate
 FROM wal.store_growth_rate
